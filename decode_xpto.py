67cdbe23b9bdff43de0c047de61ac24a506c3289
vit0r <vitornascimentoaraujo@gmail.com>
2019-08-07 23:29:22 -0300

    fix code to load file

--- decode_xpto.py
+++ decode_xpto.py
@@ -1,15 +1,15 @@
 import hashlib
 import asyncio
-import sys
-import time
 import concurrent.futures
 import traceback
 import signal
+import aiofiles
+from os import sys
 from pathlib import Path
+
 
 max_workers = len(hashlib.algorithms_available)
 _, xpto, wordlist_file = sys.argv
-wordlist_file_lines = Path(wordlist_file).open(mode='r').readlines()
 
 
 def logging_handler(type, exception, tb):
@@ -18,48 +18,43 @@ def logging_handler(type, exception, tb):
 
 
 def create_queue_algorithms():
-    for algorithm in hashlib.algorithms_available:
-        yield algorithm
+    yield from hashlib.algorithms_available
 
 
-def create_hashes(algorithm):
-    for word in wordlist_file_lines:
+def create_hashes(algorithm, wordlist_file_contents):
+    for word in wordlist_file_contents:
         algorithm_func = hashlib.new(algorithm)
         algorithm_func.update(bytes(word.strip(), 'utf-8'))
         if getattr(algorithm_func, 'hexdigest'):
             hexdigest = str(algorithm_func.hexdigest())
-            print('=== {} === \n\n hash: {} \n'.format(algorithm, hexdigest))
-            time.sleep(0.5)
             if hexdigest == xpto:
-                password_xpto = 'XPTO_PASS: {}'.format(word)
-                print(password_xpto)
+                password_xpto = 'XPTO_PASS...: {}'.format(word)
                 Path('password.txt').open(mode='w+').write(password_xpto)
 
 
-def done_callback(algorithm):
-    print('done_callback: ', algorithm)
-    time.sleep(3)
+def done_callback(future_task):
+    if future_task:
+        print('done_callback: ', future_task._state)
 
-def signal_handler():
-    loop.stop()
-    loop.close()
-    sys.exit(1)
 
 async def main():
+    wordlist_file_contents = None
+    async with aiofiles.open(wordlist_file, mode='r') as f:
+        wordlist_file_contents = await f.readlines()
     with concurrent.futures.ProcessPoolExecutor(max_workers=max_workers) as pp_executor:
-        future_tasks = {pp_executor.submit(create_hashes, algorithm) for algorithm in create_queue_algorithms()}
+        future_tasks = {pp_executor.submit(create_hashes, algorithm, wordlist_file_contents) for algorithm in create_queue_algorithms()}
         for future_task in future_tasks:
             future_task.add_done_callback(done_callback)
 
 
-sys.excepthook = logging_handler
-
 if __name__ == '__main__':
-    start = time.time()
-    loop = asyncio.get_event_loop()
-    future = asyncio.ensure_future(main())
-    loop.set_exception_handler(logging_handler)
-    signal.signal(signal.SIGINT, signal_handler)
-    signal.signal(signal.SIGTERM, signal_handler)
-    loop.run_until_complete(future)
-    print('end', time.time() - start)
+    event_loop = asyncio.get_event_loop_policy().get_event_loop()
+    event_loop.set_exception_handler(logging_handler)
+    event_loop.set_default_executor()
+    event_loop.set_debug(True)
+    event_loop.run_until_complete(main())
+    asyncgens = event_loop.shutdown_asyncgens()
+    event_loop.run_until_complete(asyncgens)
+    event_loop.stop()
+    event_loop.close()
+    print('event_loop result: ', event_loop._clock_resolution)
